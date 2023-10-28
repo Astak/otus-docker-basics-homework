963b38010677cc32718231e5226ff81141044087
Юрий Астахов <y.astakhov@urent.city>
2023-10-28 06:12:08 +0300

    added prometheus

--- src/router/router.go
+++ src/router/router.go
@@ -1,6 +1,7 @@
 package router
 
 import (
+	ginprometheus "github.com/Astak/go-gin-prometheus"
 	"github.com/Astak/otus-docker-basics-homework/web-service-gin/handler"
 	"github.com/gin-gonic/gin"
 )
@@ -8,6 +9,10 @@ import (
 func SetupRouter(handler *handler.Handler) *gin.Engine {
 	engine := gin.Default()
 	engine.GET("/health", handler.GetHealth)
+
+	p := ginprometheus.NewPrometheus("gin")
+	p.Use(engine)
+
 	engine.GET("/user/:id", handler.GetUser)
 	engine.POST("/user", handler.CreateUser)
 	engine.DELETE("/user/:id", handler.DeleteUser)
