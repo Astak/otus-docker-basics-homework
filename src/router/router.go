bffa9c4416d19cb8383d49dd10eb7bd33926f728
Юрий Астахов <y.astakhov@urent.city>
2023-11-05 18:51:21 +0300

    fixed metrics

--- src/router/router.go
+++ src/router/router.go
@@ -1,6 +1,8 @@
 package router
 
 import (
+	"regexp"
+
 	ginprometheus "github.com/Astak/go-gin-prometheus"
 	"github.com/Astak/otus-docker-basics-homework/web-service-gin/handler"
 	"github.com/gin-gonic/gin"
@@ -11,6 +13,10 @@ func SetupRouter(handler *handler.Handler) *gin.Engine {
 	engine.GET("/health", handler.GetHealth)
 
 	p := ginprometheus.NewPrometheus("gin")
+	re := regexp.MustCompile(`\/?user\/(\d+)`)
+	p.ReqCntURLLabelMappingFn = func(c *gin.Context) string {
+		return re.ReplaceAllString(c.Request.URL.Path, "/user/:id")
+	}
 	p.Use(engine)
 
 	engine.GET("/user/:id", handler.GetUser)
